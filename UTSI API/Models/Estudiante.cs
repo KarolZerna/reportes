6e67c266f10a340dcd42e27b5e62e49d92c3326f
unknown <ddcrenegmail.com>
2022-05-23 17:59:16 +0200

    API

---
+++ UTSI API/Models/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace UTSI_API.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Genero { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Nacionalidad{ get; set; }
+        public string Estado { get; set; }
+
+
+        public int IdProcedenciaAcademica { get; set; }
+    }
+}
---
+++ UTSI API/Models/ProcedenciaAcademica.cs
@@ -0,0 +1,16 @@
+﻿namespace UTSI_API.Models
+{
+    public class ProcedenciaAcademica
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Genero { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Estado { get; set; }
+
+
+        public int IdProcedenciaAcademica { get; set; }
+    }
+}
