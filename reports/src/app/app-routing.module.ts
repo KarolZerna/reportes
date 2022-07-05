a88d92c9829df8b07d07ab5ca4c472635ea5791e
KarolZerna <karol.zern@gmail.com>
2022-07-05 10:05:09 -0400

    ..

--- reports/src/app/app-routing.module.ts
+++ reports/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IdiomasComponent } from './idiomas/idiomas.component';
+import {PastoralComponent} from './pastoral/pastoral.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent, pathMatch: 'full'},
   {path: 'idiomas', component: IdiomasComponent, pathMatch: 'full'},
+  {path: 'pastoral', component: PastoralComponent, pathMatch: 'full'},
   {path: '**', redirectTo: '/inicio'}
 ];
 
