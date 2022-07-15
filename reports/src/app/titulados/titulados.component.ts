11c6b3733bd71ea0f2de0bbaaef40a4a727dd95e
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-07-14 22:43:36 -0400

    Added Filter of table json titulados

--- reports/src/app/titulados/titulados.component.ts
+++ reports/src/app/titulados/titulados.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RestTituladosService } from './rest-titulados.service';
-import { Titulados } from './titulados';
+import { Titulados } from './tituladosClass';
+
+
+interface Sede {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-titulados',
@@ -8,6 +14,12 @@ import { Titulados } from './titulados';
   styleUrls: ['./titulados.component.css']
 })
 export class TituladosComponent implements OnInit {
+  sedes: Sede[] = [
+    {value: 'LA PAZ', viewValue: 'Sede La Paz'},
+    {value: 'COCHABAMBA', viewValue: 'Sede Cochabamba'},
+    {value: 'TARIJA', viewValue: 'Sede Tarija'},
+  ];
+
 
   title = "Titulados with json"
   constructor(private rs : RestTituladosService ){}
@@ -20,13 +32,14 @@ export class TituladosComponent implements OnInit {
 
 
   titulados : Titulados [] = [] ;
-
+  completedDataTitulados : Titulados [] = [] ;
   ngOnInit(): void {
     this.rs.getTitulados().subscribe
     (
       (response)=>
       {
-        this.titulados = response;
+        this.completedDataTitulados = response;
+        this.titulados = this.completedDataTitulados;
       },
 
       (error)=>
@@ -34,5 +47,12 @@ export class TituladosComponent implements OnInit {
         console.log("Error Occured : " + error);
       }
     )
+  }
+
+  onselect(item: any) {
+    console.log("el valor ingresado fue : " + item.value);
+    console.log("ANTES DE FILTRAR : " + this.titulados);
+    this.titulados = this.completedDataTitulados.filter( titulado => titulado.sede == item.value)
+    console.log("DESPUES DE FILTRAR: " + this.titulados);
   }
 }
