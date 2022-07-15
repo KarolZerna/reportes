184867851401c6fae8c4edf50cf558d6b4152296
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-07-14 22:43:36 -0400

    Added Filter of table json titulados

--- reports/src/app/titulados/rest-titulados.service.ts
+++ reports/src/app/titulados/rest-titulados.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Titulados} from './titulados'
+import { Titulados} from './tituladosClass'
 @Injectable({
   providedIn: 'root'
 })
