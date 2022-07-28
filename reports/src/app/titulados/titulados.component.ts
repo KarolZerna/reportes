58f188b988d4ea7b7f69a2b8df53a4b3aa055bec
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-07-28 08:49:01 -0400

    added filter careers according sede - total sum

--- reports/src/app/titulados/titulados.component.ts
+++ reports/src/app/titulados/titulados.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RestTituladosService } from './rest-titulados.service';
 import { Titulados } from './tituladosClass';
 
+import { MatSelectChange } from '@angular/material/select';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color } from 'ng2-charts';
+import { Label } from 'ng2-charts';
+
+import { Chart } from 'chart.js';
 
 interface ValueFilterInterface {
   value: string;
@@ -13,20 +19,19 @@ interface ValueFilterInterface {
   templateUrl: './titulados.component.html',
   styleUrls: ['./titulados.component.css']
 })
+
+
 export class TituladosComponent implements OnInit {
 
-  title = "Titulados with json"
   constructor(private rs : RestTituladosService ){}
 
   columns = ["sede","grado","carrera","carnetEstudiante","planAcademico","cantidadSemestres","tipoTitulacion","urlTrabajoTitulacion","estado","informacionEmpleo"];
   index : (keyof Titulados) [] = ["sede","grado","carrera","carnetEstudiante","planAcademico","cantidadSemestres","tipoTitulacion","urlTrabajoTitulacion","estado","informacionEmpleo"];
 
-  sede : keyof Titulados = 'sede';
-  index2 = [this.sede]
-
 
   titulados : Titulados [] = [] ;
   completedDataTitulados : Titulados [] = [] ;
+  idChar  = document.getElementById("bar-chart") as HTMLCanvasElement
   ngOnInit(): void {
     this.rs.getTitulados().subscribe
     (
@@ -41,6 +46,10 @@ export class TituladosComponent implements OnInit {
         console.log("Error Occured : " + error);
       }
     )
+
+
+    
+
   }
 
   sedes: ValueFilterInterface[] = [
@@ -57,6 +66,7 @@ export class TituladosComponent implements OnInit {
     {value: 'INGENIERÍA QUÍMICA', viewValue: 'INGENIERÍA QUÍMICA'},
   ];
   periodos: ValueFilterInterface[] = [
+    {value: '----', viewValue: 'Todos'},
     {value: '2006', viewValue: '2006'},
     {value: '2010', viewValue: '2010'},
     {value: '2009', viewValue: '2009'},
@@ -65,16 +75,92 @@ export class TituladosComponent implements OnInit {
     {value: '2001', viewValue: '2001'},
   ];
 
+  optionSelected : string = '----';
   filterBySede(item: any) {
     console.log("el valor ingresado fue : " + item.value);
+    this.optionSelected = item.value;
     this.titulados = this.completedDataTitulados.filter(titulado => titulado.sede == item.value)
+    //this.loadDataGraph()
+    let [careersFiltered, quantityCareers] = this.filterAccordingSede_Carreras(item.value)
+    console.log( "careersFiltered: " + careersFiltered )
+    console.log( "quantityCareers: " + quantityCareers )
+    this.loadDataGraph_withCareersFiltered(careersFiltered,quantityCareers)
   }
   filterByCarrera(item: any) {
     console.log("el valor ingresado fue : " + item.value);
-    this.titulados = this.titulados.filter(titulado => titulado.carrera == item.value)
+    this.optionSelected = item.value;
+    this.titulados = this.completedDataTitulados.filter(titulado => titulado.carrera == item.value)
+    this.loadDataGraph()
   }
-  filterByPeriodo(item: any) {
+  filterByPlanAcademico(item: any) {
     console.log("el valor ingresado fue : " + item.value);
-    this.titulados = this.titulados.filter(titulado => titulado.planAcademico == item.value)
+    this.optionSelected = item.value;
+    this.titulados = this.completedDataTitulados.filter(titulado => titulado.planAcademico == item.value);
+    this.loadDataGraph()
   }
+
+  public colores = ['#FFA500','#052854'];
+	public labels = ["Varones","Mujeres"];
+
+
+  public barChartData: ChartDataSets[] = [];
+  public barChartLabels: Label[] = [];
+  public barChartColors: Color[] = [];
+  public barChartLegend = true;
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [];
+  public barChartOptions = {
+    scales: {
+      yAxes: [{display: true, ticks: {beginAtZero: true}}]
+    }
+  };
+  loadDataGraph_withCareersFiltered(careersFiltered : any,quantityCareers: any){
+    // Try my own of graph
+    var labelDinamic  = 'Cantidad de estudiantes en : ' + this.optionSelected ;
+    console.log("el valor de labelDinamic : " + labelDinamic);
+    this.barChartData  = [
+      { data: quantityCareers , label: 'Total de Titulados'}
+    ];
+    this.barChartLabels= careersFiltered ;
+    this.barChartColors = [
+      {
+        borderColor: 'black',
+        backgroundColor: 'rgba(155,255,0,0.28)',
+      },
+    ];
+    this.barChartLegend = true;
+    this.barChartPlugins = [];
+  }
+
+  loadDataGraph(){
+    // Try my own of graph
+    var labelDinamic  = 'Cantidad de estudiantes en : ' + this.optionSelected ;
+    console.log("el valor de labelDinamic : " + labelDinamic);
+    this.barChartData  = [
+      { data: [80,90,98,87,49,75], label: labelDinamic},
+      { data: [85, 72, 78, 75, 77, 75], label: 'Top 2' },
+      { data: [85, 72, 78, 75, 77, 75], label: 'Top 3' }
+    ];
+    this.barChartLabels= ['Ing.Sistemas', 'February', 'March', 'April', 'May', 'June'];
+    this.barChartColors = [
+      {
+        borderColor: 'black',
+        backgroundColor: 'rgba(155,255,0,0.28)',
+      },
+    ];
+    this.barChartLegend = true;
+    this.barChartPlugins = [];
+  }
+
+  filterAccordingSede_Carreras( sedeSelected : string){
+    let TituladosSede = this.completedDataTitulados.filter(titulado => titulado.sede == sedeSelected) ;
+    let listaCarreras = [...new Set(TituladosSede.map(json => json.carrera))];
+    let quantityCarreras = [];
+    for( let index in listaCarreras){
+      console.log("listaCarreras" + listaCarreras[index] );
+      quantityCarreras.push(TituladosSede.filter( titulado => titulado.carrera == listaCarreras[index] ).length);
+    }
+   return [listaCarreras,quantityCarreras];
+  }
+
 }
